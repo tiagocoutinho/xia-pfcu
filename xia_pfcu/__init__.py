8bacce5aacbe457b54eb18e9532e6e6c0ff8da5d
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-09-14 11:09:22 +0200

    Bump version: 0.1.0 → 1.0.0

--- xia_pfcu/__init__.py
+++ xia_pfcu/__init__.py
@@ -2,4 +2,4 @@ from .connection import connection_for_url
 from .protocol import Protocol, protocol_for_url, ShutterStatus, FilterStatus, PFCUError, BROADCAST
 from .pfcu import PFCU
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
